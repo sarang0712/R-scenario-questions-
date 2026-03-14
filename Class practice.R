ba13d6554fde1ffd111a2c52073a05be42c1d041
Sarang Gujarathi <saranggujarathi79@gmail.com>
2026-03-14 23:03:35 +0530

    Update Class practice.R

--- Class practice.R
+++ Class practice.R
@@ -364,3 +364,360 @@ flights |>
   group_by(origin) |> 
   
   summarise(across(where(is.numeric),mean,na.rm=TRUE))
+
+
+library(tidyverse)
+library(dplyr)
+
+colSums(is.na(flights))
+
+flights |> glimpse() |> 
+  
+  mutate(total_time =sum(dep_time,arr_time)) |> 
+  
+  select(carrier,dep_time,arr_time,total_time) |> head()
+
+flights |>   
+  
+  mutate(total_time =rowSums(across(ends_with("delay")),na.rm=T)) |> 
+  
+  select(ends_with("delay"),total_time) |> tail() |>  head() 
+
+A <- flights |>   
+  
+  mutate(total_time =rowSums(across(starts_with("dep")),na.rm=T)) |> 
+  
+  select(starts_with("dep"),total_time)  |>  head() |> tail()
+
+A 
+View(A)
+
+
+View(flights)
+#prove the factor
+
+sales <-tibble(
+  
+  item=c("apple","banna","cherry"),
+  
+  jan=c(50,0,0),
+  
+  feb=c(60,80,0),
+  
+  mar=c(20,NA,0),
+  
+  apr=c(20,NA,0),
+  
+)
+
+sales
+
+sales |> 
+  
+  mutate(tot = sum(jan,feb,mar),
+         
+         tot1= jan+feb+mar,
+         
+         tot2=rowSums(across(c(jan,feb,mar)),na.rm=T))
+
+sales |> 
+  
+  mutate(tot = sum(jan,feb,mar),
+         
+         tot1= jan+feb+mar,
+         
+         tot2=rowSums(across(feb:mar),na.rm=T)
+         
+  )
+
+sales |> 
+  
+  mutate(tot = sum(jan,feb,mar),
+         
+         tot1= jan+feb+mar,
+         
+         tot2=rowSums(across(ends_with("delay")),na.rm=T)
+         
+  )
+
+flights |> select(sched_dep_time,everything())
+
+flights |>   
+  
+  mutate(total_time =rowSums(across(starts_with("dep")),na.rm=T)) |> 
+  
+  select(starts_with("dep"),total_time) |> 
+  
+  filter(rowSums(across(starts_with("dep")),na.rm=T) <400)
+
+# ~(tide) in dplyr formula operator
+
+#is.character -- attribute
+
+  flights |> 
+    
+    mutate(across(where(is.character),~tolower(.x))) |> 
+    
+    select(where(is.character))
+
+# ~ apply this rule or start the formule
+
+#.x the data in this columns
+
+#. current position
+
+#find the flights where either the dep_delay or arr_delay was 
+
+#greater then 80 mintues
+
+#dep_delay+arr_delay > 80
+
+flights |> select(carrier,dep_delay,arr_delay) |> 
+  
+  mutate(delay_tot=rowSums(across(c(dep_delay,arr_delay)),na.rm = T)) |> 
+  
+  filter(delay_tot > 80)
+
+#conver the ~ formula operator
+
+flights |> select(carrier,dep_delay,arr_delay) |> 
+  
+  filter(rowSums(across(c(dep_delay,arr_delay),~.x>80),na.rm = TRUE)>=1)
+
+
+sales <-tibble(
+  
+  item=c("apple","banna","cherry"),
+  
+  price=c(20,150,15),
+  
+  price1=c(20,11,15)
+  
+)
+
+sales
+
+#exp
+
+sales |> 
+  
+  mutate(is_expensive=across(c(price,price1),~.x>100),
+         
+         total = rowSums(across(c(price,price1),~.x>100))
+         
+  )
+
+sales <-tibble(
+
+  item=c("apple","banna","cherry"),
+
+  price=c(20,150,15),
+
+  price1=c(20,11,15)
+
+)
+
+sales
+ 
+#hites
+ 
+#hitesh will do more exp
+sales |> mutate(total15 = rowSums(across(c(price,price1),~.x>14)) > 1)
+
+sales |> 
+
+  mutate(is_expensive=across(c(price,price1),~.x>100),
+
+         total = rowSums(across(c(price,price1))),
+
+         total1 = rowSums(across(c(price,price1),~.x>162)),
+
+         total2 = rowSums(across(c(price,price1),~.x>150)), #observe this
+
+         total3 = rowSums(across(c(price,price1),~.x>=20)),
+
+         total4 = rowSums(across(c(price,price1),~.x*20)), #observe this value
+
+         total5 = rowSums(across(c(price,price1),~.x>14)), 
+
+         ) |> 
+
+  filter(rowSums(across(c(price,price1),~.x>=20))>1)
+
+  #filter(total3 >1)
+ 
+a <- TRUE
+
+as.integer(a)
+
+a <- FALSE
+
+as.integer(a)
+
+FALSE+FALSE
+
+FALSE+TRUE
+
+TRUE+TRUE
+ 
+#jyothi issues
+dm<-read_csv("C:\\Users\\ganes\\Downloads\\dm.csv")
+glimpse(dm)
+
+dm |> filter(!is.na(SEX)) |> 
+  filter(
+    ARM %in% c("Treatment 3", "Treatment 2") &
+      (AGE >= 18)
+  ) |>
+  select(STUDYID, USUBJID, ARM, AGE, SEX, RACE) |>
+  arrange(ARM, USUBJID)  |> slice_head(n = 10)
+
+#find the flights where either the dep_delay or arr_delay was greater then 80 mintues
+
+flights |>  select(carrier,dep_delay,arr_delay) |> 
+  
+  
+  filter(rowSums(across(c(dep_delay,arr_delay),~.x>80),na.rm = TRUE)>=2)
+
+#filtering with comparison operator
+
+#== equal to 
+
+flights |> 
+  
+  filter(dest=='MIA') |> select(dest, everything())
+
+#!= -- not equal operator
+
+flights |> 
+  
+  filter(origin !='EWR') |> distinct(origin)
+
+#> 
+
+flights |> 
+  
+  filter(dep_delay >120)
+
+# < , <=,>=
+
+flights |> 
+  
+  filter(dep_delay < 120)
+
+# "in group" %in%
+
+flights |> 
+  
+  filter(carrier %in% c("UA","AA","DL")) |> distinct(carrier)
+
+#relational &, | and !NOT
+
+flights |> 
+  
+  filter(month==1 & day==1) |> select(carrier,month,day)
+
+#
+
+flights |> 
+  
+  filter(month==1 | month==11) |> select(carrier,month,day)
+
+#!
+
+#flights where it is not delayed more then 2 hours
+
+flights |> filter(!(arr_delay >120)) |> distinct(arr_delay)
+
+##techniques to find the missing values
+
+is.na(flights$arr_delay)
+
+#colSums
+
+missing_col <- colSums(is.na(flights)) 
+
+missing_col[missing_col>0]
+
+#dply way
+
+flights |> 
+  
+  summarise(across(everything(),~sum(is.na(.x)))) |> glimpse()
+
+#data.table this fastest data retrival
+
+library(data.table)
+
+flight_dt <- as.data.table(flights)
+
+flight_dt[,lapply(.SD, function(x) sum(is.na(x)))]
+
+#naniar
+
+library(naniar)
+
+gg_miss_var(flights)
+
+#flights which are cancelled 
+
+flights |> filter(dep_time==NA) #it does not work
+
+flights |> filter(is.na(dep_time))
+
+#flights which are departured only
+
+flights |> filter(!is.na(dep_time))
+
+#flights which cancelled out effectively
+
+flights |> filter(is.na(dep_time) & is.na(arr_time)) |> select(carrier,dep_time,arr_time)
+
+#remove NA rows
+
+flights |> filter(!is.na(dep_time) & !is.na(arr_time)) |> select(carrier,dep_time,arr_time)
+
+#other effective
+
+flights |> drop_na(dep_time,arr_time) |> select(carrier,dep_time,arr_time)
+
+3>60
+
+0 > 60
+
+#flights were arrival delay > 60 minutes
+
+flights |> drop_na(arr_delay) |> filter(arr_delay >60) |> select(carrier,dep_time,arr_time,arr_delay)
+
+#using coalesce()
+
+flights |> mutate(n_a =coalesce(arr_delay,1)) |>
+  select(carrier,dep_time,arr_time,arr_delay,n_a) |>  head(15)
+
+flights |> mutate(n_a =coalesce(arr_delay,1)) |> 
+  select(carrier,dep_time,arr_time,arr_delay,n_a) |> filter(is.na(arr_delay))
+                  
+                  
+flights |>drop_na(arr_delay) |>  
+  select(carrier, dep_time, arr_time, arr_delay) 
+
+sum(is.na(flights$arr_delay))
+                  
+                  
+#mutliple columsn if departure delay is missing, it usses the arrival delay
+
+flights |> filter(is.na(dep_delay) | !is.na(arr_delay)) |> 
+  
+  mutate(delay = coalesce(dep_delay,arr_delay)) |> select(carrier,dep_delay,arr_delay,delay)
+
+
+C <- flights |>  mutate(arr_delay1 =coalesce(dep_time,1) & coalesce(arr_time,1) & coalesce(arr_delay,1) ) |> 
+  select(carrier,dep_time,arr_time,arr_delay, arr_delay1) 
+print(C)
+View(C)
+
+
+f <-flights |>  mutate(across(c(dep_time,arr_time,arr_delay), ~coalesce(.x,1))) |> 
+  select(carrier,dep_time,arr_time,arr_delay)
+print(f)
+View(f)
